2789d8cce084a581a28240b767d8ea59ac36097d
shakir757 <shakir757@mail.ru>
2020-12-08 17:26:44 +0300

    Toast

--- app/src/main/java/com/example/projectforkarina/MainActivity.java
+++ app/src/main/java/com/example/projectforkarina/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +16,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button buttonChangeText = findViewById(R.id.button_change_text);
+        Button buttonToast = findViewById(R.id.button_toast);
         final TextView textView = findViewById(R.id.textView);
+
 
         buttonChangeText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    textView.setText("Hi!");
+                textView.setText("Hi!");
+            }
+        });
+
+        // Чтобы легко написать setOnClickListener не запоминая и переписывая весь код,
+        // тебе нужно написать id кнопки (buttonToast например), поставить точку, написать
+        // setOnClickListener, открыт скобку и написать new View.On... и дальше всё само выйдет
+        // Жмёшь enter и все пишется за тебя
+        buttonToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Мяу!", Toast.LENGTH_LONG).show();
+                // это - тост, аля уведомление. Toast - объект, пишешь после через точку функцию makeText
+                // В скобках указываешь контекст (это то где происходит действие), то есть
+                // MainActivity.this, после то что нужно вывести, и длинну тоста
+                // Toast.LENGTH_LONG или LENGTH_SHORT, после пишешь .show();
             }
         });
     }
