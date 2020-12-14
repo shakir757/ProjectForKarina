aa3087202d1d9f10ce674981e5c44347f0f5b3a1
shakir757 <shakir757@mail.ru>
2020-12-14 21:28:37 +0300

    GafActivity

---
+++ app/src/main/java/com/example/projectforkarina/GafActivity.java
@@ -0,0 +1,14 @@
+package com.example.projectforkarina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GafActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gaf);
+    }
+}
